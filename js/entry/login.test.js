3e7c36dcc5d5c63dd7b34dbd862f7ba7fa0df77b
runeunhjem <runeunhjem@gmail.com>
2023-11-06 10:45:28 +0100

    Adds check of fetchLocalStorage to loginTest

--- js/entry/login.test.js
+++ js/entry/login.test.js
@@ -1,8 +1,12 @@
 import { loginUser } from "./login.js";
-import { setLocalStorage } from "../localStorage/localStorage.js";
+import { setLocalStorage, fetchLocalStorage } from "../localStorage/localStorage.js";
 import { apiCall } from "../api/api.js";
+// import { fetchLocalStorage } from "../localStorage/localStorage.js"; // Import the fetchLocalStorage function
 
-jest.mock("../localStorage/localStorage.js", () => ({ setLocalStorage: jest.fn() }));
+jest.mock("../localStorage/localStorage.js", () => ({
+  setLocalStorage: jest.fn(),
+  fetchLocalStorage: jest.fn(),
+}));
 jest.mock("../api/constant.js", () => ({ apiUrls: { login_Url: "mocked-api-url" } }));
 jest.mock("../api/api.js", () => ({ apiCall: jest.fn() }));
 
@@ -23,5 +27,13 @@ describe("loginUser", () => {
     // Assert that the functions were called as expected
     expect(apiCall).toHaveBeenCalledWith("mocked-api-url", "post", expect.any(String));
     expect(setLocalStorage).toHaveBeenCalledWith("token", mockAccessToken);
+
+    // Fetching the token from local storage
+    fetchLocalStorage.mockReturnValue(mockAccessToken);
+
+    const tokenFromLocalStorage = fetchLocalStorage("token");
+
+    // Asserting that the token was fetched correctly from localStorage
+    expect(tokenFromLocalStorage).toEqual(mockAccessToken);
   });
 });
